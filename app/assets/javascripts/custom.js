5e28d60a162c78529b4be70fad9e46f63f74e3d1
Avnish <ravi.ranjan@actiknow.com>
2019-06-24 12:23:45 +0530

    added page loading functionality

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -126,3 +126,9 @@ $(function() {
     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
   });
 });
+
+document.onreadystatechange = function () { 
+     if (document.readyState === 'complete') {
+       $('#pre_loader').hide();
+     }
+   }
