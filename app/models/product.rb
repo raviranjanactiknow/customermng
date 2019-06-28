795aeb62371018f5a0655252c1f59a8c75ef86bb
Avnish <ravi.ranjan@actiknow.com>
2019-06-28 15:12:44 +0530

    fix s3 issue and integrated grid design

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :supplier
   belongs_to :category
-  validates :image ,presence: true
+  #validates :image ,presence: true
   validates :recorder_level,:quantity_per_unit, :unit_in_stock, :unit_price, numericality: true
   validates :product_name, :category_id,:supplier_id, presence: true ,on: :create
   validates :product_name, presence: true ,format: {with: /[a-zA-Z]/},on: :update
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,10 @@
-
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    :provider => 'AWS',                        # required
-    :aws_access_key_id =>  ENV['ACCESS_KEY_ID'],                        # required unless using use_iam_profile
-    :aws_secret_access_key => ENV['SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
-    :region => 'us-west-2',                 # optional, defaults to 'us-east-1'
+    provider:  'AWS',                        # required
+    aws_access_key_id:  ENV['ACCESS_KEY_ID'],                        # required unless using use_iam_profile
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],                        # required unless using use_iam_profile
+    region: 'us-west-2',                 # optional, defaults to 'us-east-1'
   }
    config.fog_directory  = 'funnel-images-test' 
    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
